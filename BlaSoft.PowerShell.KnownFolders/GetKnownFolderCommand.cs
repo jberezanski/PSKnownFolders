4fe0f532fef58845b5e5a471ed62476521d68e10
Jakub Bereżański <kuba@berezanscy.pl>
2013-06-09 23:38:37 +0200

    Add PowerShell provider project

---
+++ BlaSoft.PowerShell.KnownFolders/GetKnownFolderCommand.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlaSoft.PowerShell.KnownFolders
+{
+    [Cmdlet(VerbsCommon.Get, "KnownFolder")]
+    public class GetKnownFolderCommand : Cmdlet
+    {
+    }
+}
