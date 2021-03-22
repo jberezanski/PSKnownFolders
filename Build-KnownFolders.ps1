1d94c06737f514af6d917b2bb420049df0bb783e
Jakub Bereżański <kuba@berezanscy.pl>
2021-03-23 00:38:18 +0100

    Add rudimentary build script and set proper version

---
+++ Build-KnownFolders.ps1
@@ -0,0 +1,4 @@
+﻿Remove-Item -Path .\Output -Recurse -Force
+nuget restore .\KnownFolders.sln
+msbuild /m /v:m .\KnownFolders.sln /p:Configuration=Release
+robocopy .\BlaSoft.PowerShell.KnownFolders\bin\Release .\Output\PSModules\KnownFolders /E /XF System.Management.Automation.dll
